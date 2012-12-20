fca0425caf2e629fd6f035b0a7f6038f4112eb21
Pedro Melo <melo@simplicidade.org>
2012-12-20 11:07:06 +0000

    Add a WORKSPACE_DIR ENV to our config.sh

--- etc/config.sh
+++ etc/config.sh
@@ -7,6 +7,11 @@ RELEASE_DIR="$HOME/releases"
 export RELEASE_DIR
 
 
+## A place to generate files into
+WORKSPACE_DIR="$HOME/workspace"
+export WORKSPACE_DIR
+
+
 ## Where is the qmail app?
 QMAIL_DIR="/var/qmail"
 QMAIL_CONTROL_DIR="$QMAIL_DIR/control"
@@ -14,6 +19,13 @@ export QMAIL_DIR QMAIL_CONTROL_DIR
 
 
 ### Configuration ends, sanitize our environment now ###
+
+## A non-existing workspace is not very usefull
+if [ ! -d "$WORKSPACE_DIR" ] ; then
+  mkdir -p "$WORKSPACE_DIR"
+fi
+chown $SRV_USER_ID:$SRV_GROUP_ID "$WORKSPACE_DIR"
+
 
 ## Make sure our service UID can manage all the services
 [ -d "./supervise" ] &&  chown -R $SRV_USER_ID:$SRV_GROUP_ID "./supervise"
